Fix few numerical errors

example.py
@@ -34,16 +34,17 @@ screenings = [NO_SCREEN, STATIC_SAFE, DYNAMIC_SAFE, DST3, GAPSAFE_SEQUENTIAL,
 screenings_names = ["NO SCREENING", "STATIC SAFE", "DYNAMIC SAFE", "DST3",
                     "GAP SAFE SEQUENTIAL", "GAP SAFE"]
 
-eps_ = range(4, 6, 2)
+eps_ = range(2, 8, 2)
 times = np.zeros((len(screenings), len(eps_)))
 for ieps, eps in enumerate(eps_):
     for iscreening, screening in enumerate(screenings):
 
         begin = time.time()
 
-        coefs, dual_gaps, screening_size_groups, screening_size_features, \
-            n_iters = sgl_path(X, y, size_groups, omega, screening,
-                               eps=10 ** (-eps))
+        coefs, dual_gaps, lambdas, screening_size_groups, \
+            screening_size_features, n_iters = \
+            sgl_path(X, y, size_groups, omega, screening,
+                     eps=10 ** (-eps))
 
         duration = time.time() - begin
         times[iscreening, ieps] = duration
@@ -67,5 +68,5 @@ plt.xticks(range(len(eps_)), [r"$%s$" % (np.str(t)) for t in eps_])
 plt.xlabel(r"$-\log_{10}\text{(duality gap)}$", fontsize=20)
 plt.ylabel(r"$\text{Time (s)}$", fontsize=20)
 plt.grid(color='w')
-leg = plt.legend(frameon=True, loc='center left', bbox_to_anchor=(1, 0.8))
+leg = plt.legend(loc='best')
 plt.show()

sgl.py
@@ -10,16 +10,23 @@ import numpy as np
 from sgl_fast import bcd_fast
 from sgl_tools import build_lambdas, precompute_norm, precompute_DGST3
 
-NO_SCREEN = 0
+
+NO_SCREENING = 0
+
 STATIC_SAFE = 1
 DYNAMIC_SAFE = 2
 DST3 = 3
-GAPSAFE_SEQUENTIAL = 4
+
+GAPSAFE_SEQ = 4
 GAPSAFE = 5
+
+GAPSAFE_SEQ_pp = 6
+GAPSAFE_pp = 7
 
 
 def sgl_path(X, y, size_groups, omega, screen, beta_init=None, lambdas=None,
-             tau=0.5, lambda2=0, max_iter=30000, f=10, eps=1e-4):
+             tau=0.5, lambda2=0, max_iter=30000, f=10, eps=1e-4,
+             warm_start_plus=False):
     """Compute Sparse-Group-Lasso path with block coordinate descent
 
     The Sparse-Group-Lasso optimization solves:
@@ -48,7 +55,7 @@ def sgl_path(X, y, size_groups, omega, screen, beta_init=None, lambdas=None,
     screen : integer
         Screening rule to be used: it must be choosen in the following list
 
-        NO_SCREEN = 0 : Standard method
+        NO_SCREENING = 0 : Standard method
 
         STATIC_SAFE = 1 : Use static safe screening rule
             cf. El Ghaoui, L., Viallon, V., and Rabbani, T.
@@ -66,9 +73,11 @@ def sgl_path(X, y, size_groups, omega, screen, beta_init=None, lambdas=None,
             "Learning sparse representations of high dimensional data on large
             scale dictionaries". NIPS 2011
 
-        GAPSAFE_SEQUENTIAL = 4 : Proposed safe screening rule using duality gap in a sequential way.
+        GAPSAFE_SEQ = 4 : Proposed safe screening rule using duality gap
+                                 in a sequential way.
 
-        GAPSAFE = 5 : Proposed safe screening rule using duality gap in both a sequential and dynamic way.
+        GAPSAFE = 5 : Proposed safe screening rule using duality gap in both a
+                      sequential and dynamic way.
 
     beta_init : array, shape (n_features, ), optional
         The initial values of the coefficients.
@@ -107,10 +116,12 @@ def sgl_path(X, y, size_groups, omega, screen, beta_init=None, lambdas=None,
         optimizer to reach the specified accuracy for each lambda.
 
     """
+
     n_groups = len(size_groups)
-    g_start = np.zeros(n_groups, order='F', dtype=np.intc)
-    for i in range(1, n_groups):
-        g_start[i] = size_groups[i - 1] + g_start[i - 1]
+    # g_start = np.zeros(n_groups, order='F', dtype=np.intc)
+    # for i in range(1, n_groups):
+    #     g_start[i] = size_groups[i - 1] + g_start[i - 1]
+    g_start = np.cumsum(size_groups, dtype=np.intc) - size_groups[0]
 
     if lambdas is None:
         lambdas, imax = build_lambdas(X, y, omega, size_groups, g_start)
@@ -125,15 +136,12 @@ def sgl_path(X, y, size_groups, omega, screen, beta_init=None, lambdas=None,
         nDST3, norm2_nDST3, nDST3Ty = \
             precompute_DGST3(X, y, tau, omega, lambdas[0], imax, size_groups,
                              g_start)
-        tau_w_star = tau + (1 - tau) * omega[imax]
-        nTy_on_lambda_ = nDST3Ty / lambdas[0]
+        tau_w_star = tau + (1. - tau) * omega[imax]
 
     else:  # We take arbitrary values since they are not used by others rules
         nDST3 = np.ones(1)
         norm2_nDST3 = 1
-        nDST3Ty = 1
         tau_w_star = 1
-        nTy_on_lambda_ = 1
 
     n_lambdas = len(lambdas)
     n_samples, n_features = X.shape
@@ -143,8 +151,8 @@ def sgl_path(X, y, size_groups, omega, screen, beta_init=None, lambdas=None,
     X = np.asfortranarray(X)
     y = np.asfortranarray(y)
     size_groups = np.asfortranarray(size_groups, dtype=np.intc)
-    norm_X = np.asfortranarray(norm_X)
-    norm_X_g = np.asfortranarray(norm_X_g)
+    norm2_X = np.asfortranarray(norm_X ** 2)
+    norm2_X_g = np.asfortranarray(norm_X_g ** 2)
     omega = np.asfortranarray(omega)
     if beta_init is None:
         beta_init = np.zeros(n_features, order='F')
@@ -154,42 +162,52 @@ def sgl_path(X, y, size_groups, omega, screen, beta_init=None, lambdas=None,
     coefs = np.zeros((n_features, n_lambdas), order='F')
     residual = np.asfortranarray(y - np.dot(X, beta_init))
     XTR = np.asfortranarray(np.dot(X.T, residual))
-    dual_scale_p = lambda_max  # good iif beta_init = 0
+    dual_scale = lambda_max  # good iif beta_init = 0
 
     dual_gaps = np.ones(n_lambdas)
     screening_sizes_features = np.zeros(n_lambdas)
     screening_sizes_groups = np.zeros(n_lambdas)
     n_iters = np.zeros(n_lambdas)
 
+    disabled_features = np.zeros(n_features, dtype=np.intc, order='F')
+    disabled_groups = np.zeros(n_groups, dtype=np.intc, order='F')
+
     for t in range(n_lambdas):
 
-        if t == 0:
-            lambda_prec = lambda_max
-        else:
-            lambda_prec = lambdas[t - 1]
+        model = bcd_fast(X, y, beta_init, XTR, residual, dual_scale, omega,
+                         n_samples, n_features, n_groups, size_groups, g_start,
+                         norm2_X, norm2_X_g, nrm2_y, tau, lambdas[t], lambda2,
+                         lambda_max, max_iter, f, eps, screen,
+                         nDST3, norm2_nDST3, tau_w_star,
+                         disabled_features, disabled_groups, wstr_plus=0)
 
-        if screen == DST3:
-            nTy_on_lambda_ = nDST3Ty / lambdas[t]
-
-        model = bcd_fast(X, y, beta_init, XTR, residual,
-                                           dual_scale_p, omega,
-                                           n_samples, n_features, n_groups,
-                                           size_groups, g_start, norm_X,
-                                           norm_X_g, nrm2_y,
-                                           tau, lambdas[t], lambda2,
-                                           lambda_prec, lambda_max, max_iter,
-                                           f, eps, screen, nDST3, norm2_nDST3,
-                                           nTy_on_lambda_, tau_w_star)
-
-        dual_scale_p, dual_gaps[t], n_active_groups, n_active_features, \
+        dual_scale, dual_gaps[t], n_active_groups, n_active_features, \
             n_iters[t] = model
-        coefs[:, t] = beta_init
-        screening_sizes_groups[t] = n_active_groups
-        screening_sizes_features[t] = n_active_features
 
-        if abs(dual_gaps[t]) > eps:
-            print("Warning did not converge ... t = %s gap = %s eps = %s" %
-                  (t, dual_gaps[t], eps))
+        coefs[:, t] = beta_init.copy()
+
+        if t == 0 and screen != NO_SCREENING:
+            screening_sizes_features[0] = 0
+            screening_sizes_groups[0] = 0
+        else:
+            screening_sizes_groups[t] = n_active_groups
+            screening_sizes_features[t] = n_active_features
+
+        if warm_start_plus and t < n_lambdas - 1 and t != 0 and \
+           (screening_sizes_features[t] < n_features or
+                screening_sizes_groups[t] < n_groups):
+
+            bcd_fast(X, y, beta_init, XTR, residual, dual_scale, omega,
+                     n_samples, n_features, n_groups, size_groups, g_start,
+                     norm2_X, norm2_X_g, nrm2_y, tau, lambdas[t + 1], lambda2,
+                     lambda_max, max_iter, f, eps, screen,
+                     nDST3, norm2_nDST3, tau_w_star,
+                     disabled_features, disabled_groups, wstr_plus=1)
+
+        if abs(dual_gaps[t]) > eps * nrm2_y:
+            print("Warning did not converge ... t = %s gap = %s \
+                   eps = %s n_iter = %s" %
+                  (t, dual_gaps[t], eps, n_iters[t]))
 
     return (coefs, dual_gaps, lambdas, screening_sizes_groups,
             screening_sizes_features, n_iters)

sgl_tools.py
@@ -8,6 +8,7 @@
 
 import numpy as np
 from scipy.linalg import toeplitz
+from sklearn.utils import check_random_state
 
 
 def vect_ST(u, x):
@@ -15,23 +16,27 @@ def vect_ST(u, x):
         Vectorial soft-thresholding at level u.
 
     """
-    return [np.sign(x[i]) * max(abs(x[i]) - u, 0) for i in range(len(x))]
+    return np.sign(x) * np.maximum(np.abs(x) - u, 0.)
 
 
-def generate_data(n_samples, n_features, size_groups, rho=0.5):
+def generate_data(n_samples, n_features, size_groups, rho=0.5,
+                  random_state=24):
     """ Data generation process with Toplitz like correlated features:
         this correspond to the synthetic dataset used in our paper
         "GAP Safe Screening Rules for Sparse-Group Lasso".
 
     """
+
+    rng = check_random_state(random_state)
     n_groups = len(size_groups)
-    g_start = np.zeros(n_groups, order='F', dtype=np.intc)
-    for i in range(1, n_groups):
-        g_start[i] = size_groups[i - 1] + g_start[i - 1]
+    # g_start = np.zeros(n_groups, order='F', dtype=np.intc)
+    # for i in range(1, n_groups):
+    #     g_start[i] = size_groups[i - 1] + g_start[i - 1]
+    g_start = np.cumsum(size_groups, dtype=np.intc) - size_groups[0]
 
     # 10% of groups are actives
     gamma1 = int(np.ceil(n_groups * 0.1))
-    selected_groups = np.random.random_integers(0, n_groups - 1, gamma1)
+    selected_groups = rng.random_integers(0, n_groups - 1, gamma1)
     true_beta = np.zeros(n_features)
 
     for i in selected_groups:
@@ -40,21 +45,18 @@ def generate_data(n_samples, n_features, size_groups, rho=0.5):
         end = g_start[i] + size_groups[i]
         # 10% of features are actives
         gamma2 = int(np.ceil(size_groups[i] * 0.1))
-        selected_features = np.random.random_integers(begin, end - 1, gamma2)
+        selected_features = rng.random_integers(begin, end - 1, gamma2)
 
         ns = len(selected_features)
-        s = 2 * np.random.rand(ns) - 1
-        u = np.random.rand(ns)
+        s = 2 * rng.rand(ns) - 1
+        u = rng.rand(ns)
         true_beta[selected_features] = np.sign(s) * (10 * u + (1 - u) * 0.5)
 
     vect = rho ** np.arange(n_features)
     covar = toeplitz(vect, vect)
 
-    X = np.random.multivariate_normal(np.zeros(n_features), covar, n_samples)
-    y = np.dot(X, true_beta) + 0.01 * np.random.normal(0, 1, n_samples)
-
-    X /= np.sqrt(np.sum(X ** 2, axis=0))
-    y /= np.linalg.norm(y, ord=2)
+    X = rng.multivariate_normal(np.zeros(n_features), covar, n_samples)
+    y = np.dot(X, true_beta) + 0.01 * rng.normal(0, 1, n_samples)
 
     return X, y
 
@@ -125,11 +127,11 @@ def precompute_norm(X, y, size_groups, g_start):
     n, p = X.shape
     n_groups = len(size_groups)
 
-    norm_X = [np.linalg.norm(X[:, j], ord=2) for j in range(p)]
+    norm_X = np.linalg.norm(X, axis=0)
     norm_X_g = [np.linalg.norm(X[:, g_start[i]:g_start[i] + size_groups[i]])
                 for i in range(n_groups)]
 
-    return norm_X, norm_X_g, nrm2_y
+    return norm_X, np.array(norm_X_g), nrm2_y
 
 
 def precompute_DGST3(X, y, tau, omega, lambda_max, imax, size_groups, g_start):
@@ -138,16 +140,16 @@ def precompute_DGST3(X, y, tau, omega, lambda_max, imax, size_groups, g_start):
 
     """
     g_max = range(g_start[imax], g_start[imax] + size_groups[imax])
-    tgmax = (1 - tau) * omega[imax] / (tau + (1 - tau) * omega[imax])
+    tgmax = (1. - tau) * omega[imax] / (tau + (1. - tau) * omega[imax])
     XgmaxTy_on_lambda_max = np.dot(X[:, g_max].T, y) / lambda_max
-    treshold_n = (1 - tgmax) * epsilon_norm(XgmaxTy_on_lambda_max, 1 - tgmax,
-                                            tgmax)
+    treshold_n = (1. - tgmax) * \
+        epsilon_norm(XgmaxTy_on_lambda_max, 1. - tgmax, tgmax)
 
     if tau == 1:
 
         Xty = np.dot(X.T, y)
         nDST3 = X[:, np.argmax(Xty)]
-        norm2_nDST3 = 1
+        norm2_nDST3 = 1.
         nDST3Ty = lambda_max
 
     else:
@@ -155,10 +157,10 @@ def precompute_DGST3(X, y, tau, omega, lambda_max, imax, size_groups, g_start):
         STx = vect_ST(treshold_n, XgmaxTy_on_lambda_max)
 
         scale_n = tgmax * np.linalg.norm(STx, ord=2) + \
-            (1 - tgmax) * np.linalg.norm(STx, ord=1)
+            (1. - tgmax) * np.linalg.norm(STx, ord=1)
 
         nDST3 = np.dot(X[:, g_max], STx) / scale_n
-        norm2_nDST3 = np.linalg.norm(nDST3, ord=2)
+        norm2_nDST3 = np.linalg.norm(nDST3) ** 2
         nDST3Ty = np.dot(nDST3.T, y)
 
     return nDST3, norm2_nDST3, nDST3Ty
@@ -167,21 +169,21 @@ def precompute_DGST3(X, y, tau, omega, lambda_max, imax, size_groups, g_start):
 def build_lambdas(X, y, omega, size_groups, g_start, n_lambdas=100, delta=3,
                   tau=0.5):
     """
-        Compute a list of regularization parameters which decrease geometrically
-
+        Compute a list of regularization parameters which decrease
+        geometrically
     """
-    eps_g = (1 - tau) * omega / (tau + (1 - tau) * omega)
+    eps_g = (1. - tau) * omega / (tau + (1. - tau) * omega)
     n_groups = len(size_groups)
 
     nrm = [epsilon_norm(
         np.dot(X[:, g_start[i]:g_start[i] + size_groups[i]].T, y),
-        1 - eps_g[i], eps_g[i]) for i in range(n_groups)]
+        1. - eps_g[i], eps_g[i]) for i in range(n_groups)]
 
-    nrm = np.array(nrm) / (tau + (1 - tau) * omega)
+    nrm = np.array(nrm) / (tau + (1. - tau) * omega)
     imax = np.argmax(nrm)
     lambda_max = nrm[imax]
 
     lambdas = lambda_max * \
-        10 ** (-delta * np.arange(n_lambdas) / float(n_lambdas - 1))
+        10 ** (-delta * np.arange(n_lambdas) / (n_lambdas - 1.))
 
     return lambdas, imax